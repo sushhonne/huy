2ad9d156b24ddb24e693ba3627e2ea4979651627
sushhonne <sushmithahonne12@gmail.com>
2024-02-07 18:37:08 +0530

    added lenis for smooth scroll user experience

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -1,41 +1,71 @@
-import React, { useEffect } from 'react';
-
-
+import React, { useEffect } from "react";
+import Lenis from "@studio-freight/lenis"
 function Banner() {
-
-
   useEffect(() => {
     const handleScroll = () => {
       const opacity = 1 - window.scrollY / 250;
-      document.querySelector('.customized').style.opacity = opacity > 0 ? opacity : 0;
+      document.querySelector(".customized").style.opacity =
+        opacity > 0 ? opacity : 0;
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+useEffect(()=>{
+  const lenis = new Lenis()
+
+  lenis.on('scroll', (e) => {
+    console.log(e)
+  })
+  
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+  
+  requestAnimationFrame(raf)
+},[])
 
   return (
     <section className="bg-[#d1d1c7]">
       <div
-        style={{ backgroundImage: `url("backgroundImage.jpg")` }}
-        className="parallax min-h-[200px] bg-fixed bg-center bg-no-repeat bg-cover bg-accent-200"
-      ></div>
+        // style={{ backgroundImage: `url("backgroundImage.jpg")` }}
+        className="parallax min-h-[200px] bg-fixed bg-center bg-no-repeat bg-cover bg-[#d1d1c7] relative flex items-center justify-between px-20"
+      >
+        {/* part-1 */}
+        <div className=" h-full w-full ">
+          <img src="logo.svg" width="100px" height="100px" alt="" />
+        </div>
+        {/* part-2 */}
+        <div className="flex items-start justify-between space-x-0 w-[800px]">
+          <div className="text-lg font-semibold text-[#a29e9a] flex items-start justify-start">
+            AVAILABLE FOR WORK MARCH 2024
+          </div>
+          <div className="flex flex-col text-[#393632] text-[20px] font-medium">
+            <p>About</p>
+            <p>Services</p>
+            <p>Works</p>
+            <p>Reviews</p>
+            <p>Contacts</p>
+          </div>
+        </div>
+      </div>
 
       <div
         className={`customized h-[500px] bg-[#d1d1c7] z-50  text-center w-full flex flex-col items-center justify-top space-y-10`}
       >
-        <p className="text-9xl font">
+        <p className="text-9xl font-grotesk font-extrabold text-[#393632]">
           HI THERE, <br /> I'M HUY NGUYEN.
         </p>
-        <p className="text-3xl font-inconsolata max-w-[60%]">
-          A freelance frontend developer & web designer propelling startup visions to reality.
+        <p className="text-3xl  max-w-[60%]">
+          A freelance frontend developer & web designer propelling startup
+          visions to reality.
         </p>
       </div>
-
-
     </section>
   );
 }
--- src/components/home/WhatWeDo.js
+++ src/components/home/WhatWeDo.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { useMouse } from "react-use";
+import { motion } from "framer-motion";
 
 export default function WhatWeDo() {
+  const [cursorEnabled, setCursorEnabled] = useState(false);
+
+  const parentRef = useRef(null);
+  const { elX, elY } = useMouse(parentRef);
+
+  const handleMouseEnter = () => {
+    setCursorEnabled(true);
+  };
+
+  const handleMouseLeave = () => {
+    setCursorEnabled(false);
+  };
+
   return (
-    <div className=" bg-black p-[80px]">
-      <p className="animate two text-8xl  font-bold text-[#d1d1c7] pb-10">
+    <div className="bg-black px-[80px] cursor-none">
+      <p className="animate two text-8xl font-bold text-[#d1d1c7]">
         <span>S</span>
         <span>E</span>
         <span>L</span>
@@ -18,13 +33,61 @@ export default function WhatWeDo() {
         <span>K</span>
         <span>S</span>
       </p>
-      <div className="grid grid-cols-2 gap-10 w-full h-full ">
-  
-        <img src="/img3.jpg" alt="hung" height={200} className="rounded-2xl"  />
-        <img src="/img3.jpg" alt="hung" height={200} className="rounded-2xl" />
-        <img src="/img3.jpg" alt="hung" height={200} className="rounded-2xl" />
-        <img src="/img3.jpg" alt="hung" height={200} className="rounded-2xl" />
-       
+      <div
+        ref={parentRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className="relative"
+      >
+        {cursorEnabled && (
+          <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0, opacity: 0 }}
+            transition={{
+              duration: 0.6,
+              ease: "easeInOut",
+              type: "spring",
+              stiffness: 100,
+            }}
+            style={{ left: elX - 30, top: elY - 30 }}
+            className="real-cursor z-50 w-[100px] h-[100px] bg-pink-300 rounded-full text-white"
+          >
+            <div className="flex items-center justify-center w-full h-full">
+              Explore
+            </div>
+          </motion.div>
+        )}
+        <div className="grid grid-cols-2 gap-20 w-full h-full py-20">
+          <div
+            className="image-container rounded-2xl"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <img src="/si-1.webp" alt="hung" />
+          </div>
+          <div
+            className="image-container rounded-2xl"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <img src="/si-2.webp" alt="hung" />
+          </div>
+          <div
+            className="image-container rounded-2xl"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <img src="/si-3.webp" alt="hung" />
+          </div>
+          <div
+            className="image-container rounded-2xl"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <img src="/si-04.webp" alt="hung" />
+          </div>
+        </div>
       </div>
     </div>
   );
